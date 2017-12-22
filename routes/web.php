959e6f99f3d9678be096118332d31ae33ca92105
Asif Nawaz <Asif.n@aimviz.com>
2017-12-22 15:55:05 +0500

    initializing

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+	use App\User;
+	Route::group(['prefix'=>'admin','middleware' => 'is-admin']
+	,function(){
+	Route::get('/admin', 'Admin\AdminPagesController@Backend');
+	});
+
+	Route::get('/', function () {    return view('welcome'); });
+
+	Auth::routes();
+
+	Route::get('/home', 'HomeController@index')->name('home');
+
+	// Auth::routes();
+
+	Route::get('/home', 'HomeController@index')->name('home');
+
+	Route::post('/sdsdsd', 'HomeController@login')->name('sdsdsd');
+	Route::get('/users', function(){
+		$users = User::all();
+		dd($users);
+	});
+
+Route::get('/irfan', 'HomeController@logi1n');
